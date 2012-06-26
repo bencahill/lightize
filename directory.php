f1dcc83c74c75ba3ea7fc8b91970226fa85d8530
Benjamin Cahill <ben@benjamincahill.com>
2012-06-26 17:55:41 -0500

    Split out thumbnail and db code in Directory

--- directory.php
+++ directory.php
@@ -37,24 +37,34 @@ class Directory {
 			$event = new Event( "Unknown", $this->id );
 			// ensure that the cache dir exists
 			mkdir( $this->cacheDir, 0700, true );
-			foreach ( $images as $image_name ) {
-				// instantiate the image to get EXIF info
-				$image = new Image( $image_name, $this->name );
-				$image->getInfo();
-				$db->query( "INSERT INTO image (name, rating, directoryId, info, date, eventId) VALUES ('$image->name', 0, $this->id, '".serialize( $image->info )."', '$image->date', '$event->id'" );
 
-				// create thumbnails
-				$imagePath = pathinfo( "$this->imageDir/$image->name" );
-				$gimage = new Gmagick( "$this->imageDir/$image->name" );
-				$gimage->thumbnailImage( 900, 600, true );
-				$gimage->writeImage( "$this->cacheDir/{$imagePath['filename']}_900.{$imagePath['extension']}" );
-				$gimage->thumbnailImage( 150, 150, true );
-				$gimage->writeImage( "$this->cacheDir/{$imagePath['filename']}_150.{$imagePath['extension']}" );
-			}
+			createThumbnails( $images );
+
+			addToDb( $images );
 		} elseif ( is_array( $images ) ) {
 			echo "\$images array is empty!";
 		} else {
 			echo "\$images is not an array!";
+		}
+	}
+
+	private function createThumbnails( $images ) {
+		foreach ( $images as $image_name ) {
+			$imagePath = pathinfo( "$this->imageDir/$image_name" );
+			$gimage = new Gmagick( "$this->imageDir/$image_name" );
+			$gimage->thumbnailImage( 900, 600, true );
+			$gimage->writeImage( "$this->cacheDir/{$imagePath['filename']}_900.{$imagePath['extension']}" );
+			$gimage->thumbnailImage( 150, 150, true );
+			$gimage->writeImage( "$this->cacheDir/{$imagePath['filename']}_150.{$imagePath['extension']}" );
+		}
+	}
+
+	private function addToDb( $images ) {
+		foreach ( $images as $image_name ) {
+			// instantiate the image to get EXIF info
+			$image = new Image( $image_name, $this->name );
+			$image->getInfo();
+			$db->query( "INSERT INTO image (name, rating, directoryId, info, date, eventId) VALUES ('$image->name', 0, $this->id, '".serialize( $image->info )."', '$image->date', '$event->id'" );
 		}
 	}
 
