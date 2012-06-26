07145641f8dc64537648d608717d1c0f67319ae3
Benjamin Cahill <ben@benjamincahill.com>
2012-06-26 18:54:13 -0500

    Add Image class

---
+++ image.php
@@ -0,0 +1,26 @@
+<?php
+
+class Image {
+
+	public $name;
+	private $dirName;
+	public $date;
+	public $info;
+
+	public function __construct( $name, $dirName ) {
+		$this->name = $name;
+		$this->dirName = $dirName;
+	}
+
+	public function getInfo() {
+		$info = eval( "return ".shell_exec( "exiftool -php -q -d '%s' -model -focallength -shutterspeed -aperture -iso -datetimeoriginal -exposurecompensation -lens -whitebalance -flash -shootingmode -focusmode $this->name" ) )[0];
+
+		$this->date = $info['DateTimeOriginal'];
+		unset( $info['DateTimeOriginal'] );
+
+		$this->info = $info;
+	}
+
+}
+
+?>
