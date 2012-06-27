a6cb22fef1410f90ddf2b371b2ed4b4d261e9de5
Benjamin Cahill <ben@benjamincahill.com>
2012-06-27 15:56:57 -0500

    Add fake user and password to bootstrap for ezSQL_pdo

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,7 @@
 
 spl_autoload_register();
 
-$db = new ezSQL_pdo('sqlite:'.Config::cacheDir.'/image.db');
+$db = new ezSQL_pdo('sqlite:'.Config::cacheDir.'/image.db','foo','bar');
 $db->query("PRAGMA foreign_keys = ON");
 
 ?>
