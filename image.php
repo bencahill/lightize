3f13472786cfa95f16ec84d79eba3d195dd1512e
Benjamin Cahill <ben@benjamincahill.com>
2012-06-26 21:16:58 -0500

    Fix image path in Image/getInfo()

--- image.php
+++ image.php
@@ -13,7 +13,7 @@ class Image {
 	}
 
 	public function getInfo() {
-		$info = eval( "return ".shell_exec( "exiftool -php -q -d '%s' -model -focallength -shutterspeed -aperture -iso -datetimeoriginal -exposurecompensation -lens -whitebalance -flash -shootingmode -focusmode $this->name" ) )[0];
+		$info = eval( "return ".shell_exec( "exiftool -php -q -d '%s' -model -focallength -shutterspeed -aperture -iso -datetimeoriginal -exposurecompensation -lens -whitebalance -flash -shootingmode -focusmode ".Config::imagePath."/".$dirName."/".$this->name ) )[0];
 
 		$this->date = $info['DateTimeOriginal'];
 		unset( $info['DateTimeOriginal'] );
